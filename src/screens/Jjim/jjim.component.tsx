2a097b8359777ecb49528c41811887263224e6bd
uuuuujin <yearn8546@gmail.com>
2022-05-08 18:12:26 +0900

    setting 아이콘 환경설정

--- src/screens/Jjim/jjim.component.tsx
+++ src/screens/Jjim/jjim.component.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
-import styled from 'styled-components/native';
+import React from "react";
+import { SafeAreaView, Text, View } from "react-native";
+import styled from "styled-components/native";
 
 // import {useAppDispatch, useAppSelector} from '../../../store/hooks';
 // import {ageCounter, nameChange} from '../../../store/slice/userSlice';
 
-import { SvgXml } from 'react-native-svg';
+import { SvgXml } from "react-native-svg";
 
 const Jjim = (): JSX.Element => {
   return (
