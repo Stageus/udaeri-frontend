e3b34cbd0c7932f7d3a59b186ba65b159b022771
uuuuujin <yearn8546@gmail.com>
2022-05-11 00:30:51 +0900

    feat Home 찜 엘리먼트 개발

--- src/components/home-jjimEle/home-jjimEle.component.tsx
+++ src/components/home-jjimEle/home-jjimEle.component.tsx
@@ -1,11 +1,31 @@
-import React from "react";
-import { Text } from "react-native";
-import { JjimContainer } from "./home-jjimEle.style";
+import React, { useCallback } from "react";
+import { useNavigation } from "@react-navigation/native";
+import {
+  JjimContainer,
+  IconWrap,
+  CatTitle,
+  ShopName,
+  TextContainer,
+} from "./home-jjimEle.style";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Feather from "react-native-vector-icons/Feather";
 
-export const HomeJjimEle = (): JSX.Element => {
+export const HomeJjimEle = (props): JSX.Element => {
+  const { categoryTitle, shopName } = props;
+
+  const navigation = useNavigation();
+  const movePage = useCallback(
+    () => navigation.navigate(shopName, { key: shopName }),
+    []
+  );
+
   return (
-    <JjimContainer activeOpacity={0.8}>
-      <Text>호호호호</Text>
+    <JjimContainer activeOpacity={0.8} onPress={movePage}>
+      <IconWrap></IconWrap>
+      <TextContainer>
+        <CatTitle>{categoryTitle}</CatTitle>
+        <ShopName numberOfLines={1}>{shopName}</ShopName>
+      </TextContainer>
     </JjimContainer>
   );
 };
--- src/screens/home/home.style.tsx
+++ src/screens/home/home.style.tsx
@@ -1,5 +1,27 @@
 import styled from "styled-components/native";
+import theme from "../../style/theme";
 
 export const HomeText = styled.Text`
   font-size: 30px;
+`;
+
+export const HomeContainer = styled.View`
+  padding: 0 20px;
+`;
+
+export const TopContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  // margin-top: 10px;
+`;
+
+interface TopTitleProp {
+  readonly main?: boolean;
+}
+
+export const TopTitle = styled.Text<TopTitleProp>`
+  font-size: 20px;
+  font-weight: bold;
+  color: ${(props) => (props.main ? theme.colors.main : "#000")};
 `;
