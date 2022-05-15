04e592278fe0bfeb518668e04370bbc5384523ac
hyodamon <hyojjang8685@kakao.com>
2022-05-16 08:45:59 +0900

    홈 페이지 변경

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -1,39 +1,73 @@
-import React from "react";
-import { SafeAreaView, View, ScrollView } from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View } from "react-native";
 import styled from "styled-components/native";
 
 // import {useAppDispatch, useAppSelector} from '../../../store/hooks';
 // import {ageCounter, nameChange} from '../../../store/slice/userSlice';
-import { HomeText, HomeContainer, TopContainer, TopTitle } from "./home.style";
+import { 
+  Container,
+  Header,
+  MiddleBlock
+} from "./Home.style";
 import { LongBtn } from "../../components/longBtn/longBtn.component";
 import { HomeJjimEle } from "../../components/home-jjimEle/home-jjimEle.component";
 import { SafeAreaViewComp } from "../../components/safeAreaViewComp/safeAreaViewComp";
+import { ScrollView } from "react-native-gesture-handler";
+import LargeCatEle from "../../components/LargeCatEle/LargeCatEle.component";
+import MagniBtn from "../../components/HeaderBar/MagniBtn/MagniBtn.component";
+
+const categoryList = [
+  {
+    name: "먹거리"
+  },
+  {
+    name: "놀거리"
+  },
+  {
+    name: "카페/빵집"
+  },
+  {
+    name: "술집"
+  },
+  {
+    name: "서비스"
+  },
+  {
+    name: "상점"
+  },
+  {
+    name: "미용실"
+  },
+  {
+    name: "병원"
+  },
+]
 
 const Home = (): JSX.Element => {
   return (
     <SafeAreaViewComp>
-      <HomeContainer>
-        <TopContainer>
-          <TopTitle>우리대학 거리</TopTitle>
-          <TopTitle main>인하대학교</TopTitle>
-        </TopContainer>
-        <HomeText>홈!!!~!~</HomeText>
-        <LongBtn btnTitle="지도로 보기" pageTitle="Map" />
-
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{
-            marginVertical: 15,
-            flexDirection: "row",
-          }}
-        >
-          <HomeJjimEle categoryTitle="음식점" shopName="맛사랑"></HomeJjimEle>
-          <HomeJjimEle categoryTitle="음식점" shopName="맛사랑"></HomeJjimEle>
-          <HomeJjimEle categoryTitle="음식점" shopName="맛사랑"></HomeJjimEle>
-          <HomeJjimEle categoryTitle="음식점" shopName="맛사랑"></HomeJjimEle>
+        <ScrollView showsHorizontalScrollIndicator={false}>
+          <Container>
+            <Header.Container>
+              <Header.LogoText>우리대학거리</Header.LogoText>
+              <MagniBtn title={null} size={36}></MagniBtn>
+            </Header.Container>
+            <MiddleBlock.Container>
+              <MiddleBlock.CatWrap>
+                {categoryList.map((item, index) => {
+                  return(
+                    <LargeCatEle
+                        key={index}
+                        name={item.name}
+                    ></LargeCatEle>
+                )})}
+              </MiddleBlock.CatWrap>
+              <LongBtn btnTitle="지도로 보기" pageTitle="Map" />
+            </MiddleBlock.Container>
+            
+            <HomeJjimEle></HomeJjimEle>
+          </Container>
         </ScrollView>
-      </HomeContainer>
     </SafeAreaViewComp>
   );
 };
