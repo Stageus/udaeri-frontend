7de5edd230b49b72cf5369e71d8d766bba015154
uuuuujin <yearn8546@gmail.com>
2022-05-17 14:12:40 +0900

    setting 리덕스 세팅

--- src/screens/home/home.component.tsx
+++ src/screens/home/home.component.tsx
@@ -4,11 +4,7 @@ import styled from "styled-components/native";
 
 // import {useAppDispatch, useAppSelector} from '../../../store/hooks';
 // import {ageCounter, nameChange} from '../../../store/slice/userSlice';
-import { 
-  Container,
-  Header,
-  MiddleBlock
-} from "./Home.style";
+import { Container, Header, MiddleBlock } from "./home.style";
 import { LongBtn } from "../../components/longBtn/longBtn.component";
 import { HomeJjimEle } from "../../components/home-jjimEle/home-jjimEle.component";
 import { SafeAreaViewComp } from "../../components/safeAreaViewComp/safeAreaViewComp";
@@ -18,56 +14,52 @@ import MagniBtn from "../../components/HeaderBar/MagniBtn/MagniBtn.component";
 
 const categoryList = [
   {
-    name: "먹거리"
+    name: "먹거리",
   },
   {
-    name: "놀거리"
+    name: "놀거리",
   },
   {
-    name: "카페/빵집"
+    name: "카페/빵집",
   },
   {
-    name: "술집"
+    name: "술집",
   },
   {
-    name: "서비스"
+    name: "서비스",
   },
   {
-    name: "상점"
+    name: "상점",
   },
   {
-    name: "미용실"
+    name: "미용실",
   },
   {
-    name: "병원"
+    name: "병원",
   },
-]
+];
 
 const Home = (): JSX.Element => {
   return (
     <SafeAreaViewComp>
-        <ScrollView showsHorizontalScrollIndicator={false}>
-          <Container>
-            <Header.Container>
-              <Header.LogoText>우리대학거리</Header.LogoText>
-              <MagniBtn title={null} size={36}></MagniBtn>
-            </Header.Container>
-            <MiddleBlock.Container>
-              <MiddleBlock.CatWrap>
-                {categoryList.map((item, index) => {
-                  return(
-                    <LargeCatEle
-                        key={index}
-                        name={item.name}
-                    ></LargeCatEle>
-                )})}
-              </MiddleBlock.CatWrap>
-              <LongBtn btnTitle="지도로 보기" pageTitle="Map" />
-            </MiddleBlock.Container>
-            
-            <HomeJjimEle></HomeJjimEle>
-          </Container>
-        </ScrollView>
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <Container>
+          <Header.Container>
+            <Header.LogoText>우리대학거리</Header.LogoText>
+            <MagniBtn title={null} size={36}></MagniBtn>
+          </Header.Container>
+          <MiddleBlock.Container>
+            <MiddleBlock.CatWrap>
+              {categoryList.map((item, index) => {
+                return <LargeCatEle key={index} name={item.name}></LargeCatEle>;
+              })}
+            </MiddleBlock.CatWrap>
+            <LongBtn btnTitle="지도로 보기" pageTitle="Map" />
+          </MiddleBlock.Container>
+
+          <HomeJjimEle></HomeJjimEle>
+        </Container>
+      </ScrollView>
     </SafeAreaViewComp>
   );
 };
