88798efdf57f154fcc3ea2ed3ca0431eaeea6acb
uuuuujin <yearn8546@gmail.com>
2022-05-18 14:45:02 +0900

    fix 벡터 아이콘 삭제

--- src/components/HeaderBar/BackBtn/BackBtn.component.tsx
+++ src/components/HeaderBar/BackBtn/BackBtn.component.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import Icon from "react-native-vector-icons/Ionicons";
+import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { SvgXml } from "react-native-svg";
-import { BackIcon } from '../../../assets/icon/icons';
-import theme from '../../../style/theme';
+import { BackIcon } from "../../../assets/icon/icons";
+import theme from "../../../style/theme";
 
 export type ProfileScreenNavigationProp = {
-    pop : () => void;
+  pop: () => void;
 };
 const BackBtn = () => {
-    const navigation = useNavigation<ProfileScreenNavigationProp>();
-    const onGoBack = () => {
-      navigation.pop();
-    };
-    return (
-        <SvgXml 
-            xml={BackIcon} 
-            width="24px" 
-            height="24px" 
-            fill= {theme.colors.fontColor2}
-            onPress={onGoBack}
-        />
-    )
-}
+  const navigation = useNavigation<ProfileScreenNavigationProp>();
+  const onGoBack = () => {
+    navigation.pop();
+  };
+  return (
+    <SvgXml
+      xml={BackIcon}
+      width="24px"
+      height="24px"
+      fill={theme.colors.fontColor2}
+      onPress={onGoBack}
+    />
+  );
+};
 
 export default BackBtn;
